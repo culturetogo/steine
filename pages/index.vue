e7d1c07ee4eb687f0e7edc210134847d36c86907
mari-fu-berlin <michael.mueller5@fu-berlin.de>
2018-12-18 17:41:54 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <nav class="navbar fixed-top navbar-light bg-light">
+      <button @click="$store.commit('pointerRueck')">&larr;</button>
+      <p class="anz-stein"><strong>{{ $store.state.steine[$store.state.pointer].attributes.title }}</strong></p>
+      <button @click="$store.commit('pointerVor')">&rarr;</button>
+    </nav>
+    <section class="container mt-5">
+      <div>
+        <div class="steinbild"><img
+          :src="$store.state.steine[$store.state.pointer].attributes.bildstein.url"
+          class="mx-auto d-block">
+        </div>
+        <div v-html = "$store.state.steine[$store.state.pointer].attributes.body" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    // Logo
+  },
+  data(){
+    return {
+      test: "data",
+      out: "leer",
+    }
+  },
+  methods: {
+    async fetchSomething() {
+      const ip = await this.$axios.$get('http://icanhazip.com')
+      //this.ip = ip
+      this.out = ip
+      console.log( ip )
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  text-align: left;
+}
+section {
+
+  max-width: 24rem;
+}
+.navbar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.steinbild {
+  margin-top: 2rem;
+  background: #333;
+}
+.steinbild img {
+  max-height: 16rem;
+  height: auto;
+}
+.anz-stein {
+  margin-bottom: 0;
+  padding: .6rem;
+  min-width: 8rem;
+  text-align: center;
+}
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,49 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({
+      counter: 0,
+      output: "empty",
+      testhtml: "<p>testabsatz</p>",
+      steine: [],
+      pointer: 0
+    }),
+    mutations: {
+      increment (state) {
+        state.counter++
+      },
+      ladeSteine (state, payload){
+        let steine = payload
+        state.steine = steine
+      },
+      SET_IP (state, payload) {
+        state.output = payload
+      },
+      pointerVor (state) {
+        if(state.pointer < state.steine.length -1){
+          state.pointer += 1
+        } else {
+          state.pointer = 0
+        }
+      },
+      pointerRueck (state) {
+        if(state.pointer > 0){
+          state.pointer -= 1
+        } else {
+          state.pointer = state.steine.length -1
+        }
+      }
+    },
+    actions: {
+      async nuxtServerInit ({ commit }, { $axios }) {
+        //const res = await $axios.$get('http://icanhazip.com')
+        const res = await $axios.$get('http://chorin-content.culture-to-go.de/json/steine/?sort=ordnungszahl')
+        //commit('SET_IP', res)
+        commit('ladeSteine', res.data)
+      },
+    }
+  })
+}
+
+export default createStore
