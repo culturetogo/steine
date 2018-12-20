5d7e7170cad1bb8b0af227d46bc0d4cffabeae18
mari-fu-berlin <michael.mueller5@fu-berlin.de>
2018-12-20 12:48:44 +0100

    new features

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,62 @@
 <template>
   <div>
     <nav class="navbar fixed-top navbar-light bg-light">
-      <button @click="$store.commit('pointerRueck')">&larr;</button>
-      <p class="anz-stein"><strong>{{ $store.state.steine[$store.state.pointer].attributes.title }}</strong></p>
-      <button @click="$store.commit('pointerVor')">&rarr;</button>
+      <button @click="$store.dispatch('pointerVor', -1)">&larr;</button>
+      <div class="tour-controls d-flex justify-content-around mt-3">
+        <p
+          class="tour-button"
+          @click="$store.commit('toggleTour')"><strong>{{ getTour }}</strong></p>
+        <p class="anz-stein"><strong>{{ getTitel }}</strong></p>
+      </div>
+      <button @click="$store.dispatch('pointerVor', 1)">&rarr;</button>
     </nav>
     <section class="container mt-5">
       <div>
         <div class="steinbild"><img
-          :src="$store.state.steine[$store.state.pointer].attributes.bildstein.url"
+          :src="getBildSteinUrl"
           class="mx-auto d-block">
         </div>
-        <div v-html = "$store.state.steine[$store.state.pointer].attributes.body" />
+        <div v-html = "getText" />
         <div
-          v-if="$store.state.steine[$store.state.pointer].attributes.transkription"
+          v-if="getTranskription"
           id="transkription" >
           <h3>Inschrift</h3>
           <p
-            v-html = "$store.state.steine[$store.state.pointer].attributes.transkription" />
+            v-html = "getTranskription" />
         </div>
         <div
-          v-if="$store.state.steine[$store.state.pointer].attributes.info"
+          v-if="getInfo"
           id="Info" >
           <h3>Weiterführende Information</h3>
           <p
-            v-html = "$store.state.steine[$store.state.pointer].attributes.info" />
+            v-html = "getInfo" />
         </div>
         <div
-          v-if="$store.state.steine[$store.state.pointer].attributes.bibeltext"
+          v-if="checkBibeltext && (getAudioVersion == 1)"
           id="bibeltext" >
           <h3>Bibeltext</h3>
           <p
-            v-html = "$store.state.steine[$store.state.pointer].attributes.introbibeltext" />
-          <audio controls>
+            v-html = "getIntroBibeltext" />
+          <audio
+            id="audio_01"
+            controls>
             <source
-              :src="$store.state.baseAudio + $store.state.steine[$store.state.pointer].attributes.bibeltext"
+              :src="getBibeltext"
+              type="audio/mpeg">
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div
+          v-if="checkBibeltext && (getAudioVersion == 2)"
+          id="bibeltext" >
+          <h3>Bibeltext</h3>
+          <p
+            v-html = "getIntroBibeltext" />
+          <audio
+            id="audio_02"
+            controls>
+            <source
+              :src="getBibeltext"
               type="audio/mpeg">
             Your browser does not support the audio element.
           </audio>
@@ -46,6 +68,7 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
@@ -56,6 +79,20 @@ export default {
       test: "data",
       out: "leer",
     }
+  },
+  computed: {
+    ...mapGetters([
+      'getTour',
+      'getTitel',
+      'getBildSteinUrl',
+      'getText',
+      'getTranskription',
+      'getInfo',
+      'getBibeltext',
+      'getIntroBibeltext',
+      'checkBibeltext',
+      'getAudioVersion'
+    ])
   },
   methods: {
     async fetchSomething() {
@@ -86,6 +123,9 @@ section {
   justify-content: center;
   align-items: center;
 }
+.tour-button {
+  display: block;
+}
 .steinbild {
   margin-top: 2rem;
   background: #333;
@@ -94,11 +134,11 @@ section {
   max-height: 16rem;
   height: auto;
 }
-.anz-stein {
-  margin-bottom: 0;
-  padding: .6rem;
-  min-width: 8rem;
-  text-align: center;
+.tour-controls {
+  min-width: 14rem;
+}
+.tour-button {
+  cursor: pointer;
 }
 
 </style>
--- store/index.js
+++ store/index.js
@@ -7,45 +7,111 @@ const createStore = () => {
       output: "empty",
       testhtml: "<p>testabsatz</p>",
       steine: [],
+      steine_innen: [],
+      steine_aussen: [],
+      steine_ak_tour: [],
+      tour: "Innen",
+      audio_version: 2,
       pointer: 0,
       baseAudio: "http://chorin-content.culture-to-go.de/files/"
     }),
     getters: {
+      getTour (state) {
+        return state.tour
+      },
+      getAudioVersion (state ) {
+        return state.audio_version
+      },
+      getTitel (state) {
+        return state.steine_ak_tour[state.pointer].attributes.title
+      },
+      getText (state) {
+        return state.steine_ak_tour[state.pointer].attributes.body
+      },
+      getBildSteinUrl (state) {
+        return state.steine_ak_tour[state.pointer].attributes.bildstein.url
+      },
+      getTranskription (state) {
+        return state.steine_ak_tour[state.pointer].attributes.transkription
+      },
+      getInfo (state) {
+        return state.steine_ak_tour[state.pointer].attributes.info
+      },
+      getBibeltext (state) {
+        return state.baseAudio + state.steine_ak_tour[state.pointer].attributes.bibeltext
+      },
+      getIntroBibeltext (state) {
+        return state.steine_ak_tour[state.pointer].attributes.introbibeltext
+      },
+      // Check-Funktionen
+      checkBibeltext (state) {
+        if (state.steine_ak_tour[state.pointer].attributes.bibeltext != "" ) {
+          return true
+        } else {
+          return false
+        }
 
+      }
     },
     mutations: {
       increment (state) {
         state.counter++
       },
-      ladeSteine (state, payload){
+      ladeSteine (state, payload) {
         let steine = payload
         state.steine = steine
+      },
+      splitSteine (state) {
+        for (let i = 0; i < state.steine.length; i++) {
+          if(state.steine[i].attributes.multiselect == "Innen") {
+            state.steine_innen.push(state.steine[i])
+          }
+          if(state.steine[i].attributes.multiselect == "Außen") {
+            state.steine_aussen.push(state.steine[i])
+          }
+        }
+        state.steine_ak_tour = state.steine_innen
       },
       SET_IP (state, payload) {
         state.output = payload
       },
-      pointerVor (state) {
-        if(state.pointer < state.steine.length -1){
-          state.pointer += 1
-        } else {
-          state.pointer = 0
-        }
+      pointerMove (state, payload) {
+        let pointer = state.pointer
+        pointer += payload
+        // Endlos-Durchlauf
+        pointer = pointer < 0 ? state.steine_ak_tour.length -1 : pointer
+        pointer = pointer > state.steine_ak_tour.length -1 ? pointer = 0 : pointer
+        state.pointer = pointer
+        // Reload Audio-Element bei Stein-Wechsel
+        state.audio_version = state.audio_version == 1 ? state.audio_version = 2 : state.audio_version = 1
+        console.log("mutation payload: ", payload)
       },
-      pointerRueck (state) {
-        if(state.pointer > 0){
-          state.pointer -= 1
-        } else {
-          state.pointer = state.steine.length -1
-        }
+      toggleTour (state) {
+        console.log("Tour vor Toggle: ", state.tour)
+          if(state.tour === "Innen") {
+            state.tour = "Außen"
+            state.pointer = 0
+            state.steine_ak_tour = state.steine_aussen
+          } else {
+            state.tour = "Innen"
+            state.pointer = 0
+            state.steine_ak_tour = state.steine_innen
+          }
       }
     },
     actions: {
       async nuxtServerInit ({ commit }, { $axios }) {
         //const res = await $axios.$get('http://icanhazip.com')
-        const res = await $axios.$get('http://chorin-content.culture-to-go.de/json/steine/?sort=ordnungszahl')
+        const reset = []
+        commit('ladeSteine', reset)
+        const res = await $axios.$get('http://chorin-content.culture-to-go.de/json/steine/?sort=ordnungszahl&page[size]=500')
         //commit('SET_IP', res)
         commit('ladeSteine', res.data)
+        commit('splitSteine')
       },
+      pointerVor ({ commit }, r) {
+        commit('pointerMove', r)
+      }
     }
   })
 }
