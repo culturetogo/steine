1be70abeb7413fac0cc471905d9c52f5b543bece
mclberlin <malte_cl@gmx.de>
2019-03-09 17:20:28 +0100

    Navbar component

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,6 @@
 <template>
   <div id="mmok">
-    <b-navbar
-      v-if = "getMode != 'intro'"
-      fixed = "top"
-      type="light"
-      variant="light">
-      <b-navbar-toggle target="nav_text_collapse" />
-      <b-navbar-brand
-        v-if = "getMode == 'details'"
-      >
-        <div
-          class="stein-anzeige-wrapper"
-        >
-          <b-button
-            variant="outline-dark"
-            @click="$store.dispatch('pointerVor', -1), stopAllAudio()"
-          >&lsaquo;
-          </b-button>
-          <div
-            class="anzeige-stein"
-          >
-            {{ getTour }}<br>
-            <span
-              class="stein-nr"
-            >
-              {{ getTitel }}
-            </span>
-          </div>
-          <b-button
-            variant="outline-dark"
-            @click="$store.dispatch('pointerVor', 1), stopAllAudio()"
-          >&rsaquo;
-          </b-button>
-        </div>
-      </b-navbar-brand>
-      <b-navbar-brand
-        v-if = "getMode == 'map'"
-      >
-        <div
-          class="tour-anzeige-wrapper"
-        >
-          <b-button
-            variant="outline-dark"
-            @click="$store.dispatch('toggleTour')"
-          >
-            Tour wechseln
-          </b-button>
-        </div>
-      </b-navbar-brand>
-      <b-collapse
-        id="nav_text_collapse"
-        is-nav >
-        <b-navbar-nav>
-          <b-nav-text
-            @click="backToMap"
-          >
-            Zum Plan
-          </b-nav-text>
-          <b-nav-text
-            @click="backToIntro"
-          >
-            Zur Startseite
-          </b-nav-text>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <Navbar v-if="getMode !== 'intro'"/>
     <b-container
       v-if="getMode == 'details'"
       class="main-content"
@@ -288,15 +224,16 @@
 
 <script>
 //import Logo from '~/components/Logo.vue'
+import Navbar from '~/components/Navbar.vue'
 import Intro from '~/components/Intro.vue'
 import MapModal from '~/components/modals/map-modal.vue'
 import DetailsModal from '~/components/modals/details-modal.vue'
-import Vue from 'vue'
 import { mapGetters } from 'vuex'
 
 export default {
   components: {
     // Logo
+    Navbar,
     Intro,
     'map-modal': MapModal,
     'details-modal': DetailsModal
@@ -334,13 +271,6 @@ export default {
     ])
   },
   methods: {
-    stopAllAudio() {
-     //console.log("Stop All Audio!")
-     Vue.nextTick(() => {
-       Array.from(document.getElementsByTagName('audio'))
-        .forEach(audio => audio.load());
-     });
-    },
     toggleModalMap(mi) {
       // ruft Modal mit dem ausgewählten Stein auf
       let index = mi -1
