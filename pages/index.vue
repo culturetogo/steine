631633a623b10adcbf3548356cab854b34f1b147
mclberlin <malte_cl@gmx.de>
2019-03-09 17:34:13 +0100

    page components 'map' and 'details'

--- pages/index.vue
+++ pages/index.vue
@@ -1,120 +1,7 @@
 <template>
   <div id="mmok">
     <Navbar v-if="getMode !== 'intro'"/>
-    <b-container
-      v-if="getMode == 'details'"
-      class="main-content"
-    >
-      <b-row
-        class="mmok-content"
-      >
-        <div
-          class="steinbild"
-        >
-          <img
-            :src="getBildSteinUrl"
-          >
-        </div>
-      </b-row>
-      <b-row
-        class="sub-navi"
-      >
-        <div
-          class="stein-titel"
-        >
-          {{ getTitel }}
-        </div>
-        <div>
-          <b-button
-            variant="outline-dark"
-            @click="toggleModalStone"
-          >
-            Lage des Steins
-          </b-button>
-          <b-button
-            variant="outline-dark"
-            @click="backToMap"
-          >
-            Zum Plan
-          </b-button>
-        </div>
-      </b-row>
-      <b-row
-        class="mmok-content text"
-      >
-        <div
-          v-html = "getText"
-        />
-      </b-row>
-      <b-row
-        v-if="getTranskription"
-        class="mmok-content transkription"
-      >
-        <div
-          id="transkription" >
-          <h3>Inschrift</h3>
-          <div
-            v-html = "getTranskription" />
-        </div>
-      </b-row>
-      <b-row
-        v-if="getInfo"
-        class="mmok-content info"
-      >
-        <div
-          id="Info" >
-          <h3>Weiterführende Information</h3>
-          <p
-            v-html = "getInfo" />
-        </div>
-      </b-row>
-      <b-row
-        v-if="checkBibeltext"
-        class="mmok-content bibeltext"
-      >
-        <div
-          id="bibeltext" >
-          <h3>Bibeltext</h3>
-          <p
-            v-html = "getIntroBibeltext" />
-          <audio
-            v-if="getAudio"
-            id="audio_01"
-            controls>
-            <source
-              :src="getBibeltext"
-              type="audio/mpeg">
-            Your browser does not support the audio element.
-          </audio>
-        </div>
-      </b-row>
-      <b-row
-        v-if="checkGesang"
-        class="mmok-content gesang"
-      >
-        <div
-          id="gesang" >
-          <h3>Gesang</h3>
-          <div
-            v-html = "getIntroGesang" />
-          <audio
-            v-if="getAudio"
-            id="audio_g_01"
-            controls>
-            <source
-              :src="getGesang"
-              type="audio/mpeg">
-            Your browser does not support the audio element.
-          </audio>
-        </div>
-      </b-row>
-      <b-row
-        class="mmok-content copyright"
-      >
-        <div
-          v-html="getCopyright" />
-      </b-row>
-    </b-container>
+    <Details v-if="getMode === 'details'"/>
     <b-container
       v-if="getMode == 'map'"
       class="main-content"
@@ -226,6 +113,8 @@
 //import Logo from '~/components/Logo.vue'
 import Navbar from '~/components/Navbar.vue'
 import Intro from '~/components/Intro.vue'
+import Details from './details.vue'
+import Map from './map.vue'
 import MapModal from '~/components/modals/map-modal.vue'
 import DetailsModal from '~/components/modals/details-modal.vue'
 import { mapGetters } from 'vuex'
@@ -235,6 +124,8 @@ export default {
     // Logo
     Navbar,
     Intro,
+    Details,
+    Map,
     'map-modal': MapModal,
     'details-modal': DetailsModal
   },
--- pages/map.vue
+++ pages/map.vue
@@ -1,119 +1,121 @@
 <template>
-  <div>
-    <nav class="navbar fixed-top navbar-light bg-light" />
-    <section class="container mt-5">
-      <div id="map-container">
+  <b-container
+    class="main-content"
+  >
+    <b-row
+      class="sub-navi"
+    >
+      <div
+        class="tour-titel"
+      >
+        {{ getTour }}
+      </div>
+    </b-row>
+    <b-row>
+      <div
+        v-if="getAkTour == 'Innen'"
+        id="map-container"
+      >
         <figure id="imagemap">
-          <svg viewBox="0 0 1000 1000" >
+          <svg viewBox="0 0 2000 2000" >
             <defs>
               <style>
                 rect:hover {
                 fill: green;
-                opacity:0.5;
+                opacity:0;
                 }
               </style>
             </defs>
 
             <image
-              width="1000"
-              height="1000"
-              xlink:href="~/assets/Plan_Master_01.png">
-              <title>Mount Rushmore National Memorial</title>
+              xlink:href="~/assets/Plan_Master_02.png"
+              width="2000"
+              height="2000"
+            >
+              <title>Plan</title>
             </image>
 
             <a
               v-for="(marker, index) in getMarkers"
               :key="index"
+              class="a-marker"
               xlink:href=""
-              @click="$store.dispatch('pointerTo', marker.index)"
+              @click.prevent="toggleModalMap(marker.index)"
             >
               <rect
                 :x="marker.marker_pos_left"
                 :y="marker.marker_pos_top"
                 :width="getMarker.width"
                 :height="getMarker.height"
-                opacity=".1" />
+                opacity="0" />
             </a>
           </svg>
         </figure>
       </div>
-    </section>
-  </div>
+      <div
+        v-if="getAkTour == 'Außen'"
+        id="map-container"
+      >
+        <figure id="imagemap">
+          <svg viewBox="0 0 2000 2000" >
+            <defs>
+              <style>
+                rect:hover {
+                fill: green;
+                opacity:0;
+                }
+              </style>
+            </defs>
+
+            <image
+              xlink:href="~/assets/Plan_Master_01.png"
+              width="2000"
+              height="2000"
+            >
+              <title>Plan</title>
+            </image>
+
+            <a
+              v-for="(marker, index) in getMarkers"
+              :key="index"
+              class="a-marker"
+              xlink:href=""
+              @click.prevent="toggleModalMap(marker.index)"
+            >
+              <rect
+                :x="marker.marker_pos_left"
+                :y="marker.marker_pos_top"
+                :width="getMarker.width"
+                :height="getMarker.height"
+                opacity="0" />
+            </a>
+          </svg>
+        </figure>
+      </div>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
-  components: {
-
-  },
-  data(){
-    return {
-      test: ""
-    }
-  },
   computed: {
     ...mapGetters([
+      'getTour',
+      'getAkTour',
       'getMarker',
       'getMarkers'
     ])
   },
   methods: {
-    callStone(stein) {
-
+    toggleModalMap(mi) {
+      // ruft Modal mit dem ausgewählten Stein auf
+      let index = mi -1
+      console.log("toggleMapIndex: ", index)
+      this.$store.dispatch('pointerTo', index)
+      this.$root.$emit('bv::show::modal', 'map-modal')
     }
   }
 }
 </script>
-
-<style scoped>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  text-align: left;
-}
-section {
-
-  max-width: 24rem;
-}
-.navbar {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.tour-button {
-  display: block;
-}
-.steinbild {
-  margin-top: 2rem;
-  background: #333;
-}
-.steinbild img {
-  max-height: 16rem;
-  height: auto;
-}
-.tour-controls {
-  min-width: 14rem;
-}
-.tour-button {
-  cursor: pointer;
-}
-#map-container {
-  position: relative;
-  width: 100%;
-  background: #eee;
-}
-#map-container .map {
-  width: 100%;
-}
-#map-container .marker {
-  position: absolute;
-  width: 20px;
-  top: 50%;
-  left: 33.3%;
-}
-</style>
